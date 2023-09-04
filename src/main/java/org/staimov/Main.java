f7b2779954dc9bb497185adb140c1c3da58818db
staimov <staimov@mail.ru>
2023-09-04 14:50:53 +0500

    Main updated

--- src/main/java/org/staimov/Main.java
+++ src/main/java/org/staimov/Main.java
@@ -1,7 +1,16 @@
 package org.staimov;
 
+import org.staimov.entity.*;
+import org.staimov.repository.MovieRepo;
+
 public class Main {
     public static void main(String[] args) {
+        MovieRepo repo = new MovieRepo();
 
+        Film film = repo.getFilm((short) 1);
+        System.out.println(film);
+
+        Actor actor = repo.getActor((short) 1);
+        System.out.println(actor);
     }
 }
